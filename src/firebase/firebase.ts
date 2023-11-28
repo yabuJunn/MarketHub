2dc5dc4f74cd6f13c85dd7d0c8ce4aab9824ab4c
Juan David <juancho04reyes@gmail.com>
2023-11-27 21:49:48 -0500

    Inicializada funcion de registrar usuario

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -1,4 +1,19 @@
 import { firebaseConfig } from "./firebaseConfig";
 import { initializeApp } from "firebase/app";
+import { getFirestore, collection, addDoc, updateDoc  } from "firebase/firestore";
 
 const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export const registrarUsuario = async (nameParam: string, emailParam: string, cellphoneParam: string, passwordParam: string) => {
+    const docRef = await addDoc(collection(db, "users"), {
+        name: nameParam,
+        email: emailParam,
+        cellphone: cellphoneParam,
+        password: passwordParam
+      });
+      console.log("Document written with ID: ", docRef.id);
+      await updateDoc(docRef, {
+        id: docRef.id
+      });
+}
