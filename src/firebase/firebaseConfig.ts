f9b8a254a13ecf9c3a04e33a39782dd061c0d537
Juan David <juancho04reyes@gmail.com>
2023-11-27 21:21:27 -0500

    Firebase ignorando config

--- src/firebase/firebaseConfig.ts
+++ src/firebase/firebaseConfig.ts
@@ -1,1 +1,8 @@
-
+export const firebaseConfig = {
+    apiKey: "AIzaSyDUPLn5brG4-Mh64UVuhs9KUTbJjXKx2I4",
+    authDomain: "markethub---algoritmos.firebaseapp.com",
+    projectId: "markethub---algoritmos",
+    storageBucket: "markethub---algoritmos.appspot.com",
+    messagingSenderId: "993205473828",
+    appId: "1:993205473828:web:9157c9fe17c17112f6feb1"
+};
